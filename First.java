e3230742a42807601cf1ef508af74da6cec081cc
lalitha0778 <lalithapasupuleti9@gmail.com>
2026-02-18 12:43:45 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+package TestNg;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class First {
+    @Test
+    public void openGoogle() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+
+        driver.quit();
+    }
+}
